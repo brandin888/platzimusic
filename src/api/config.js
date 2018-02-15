0b1036e6bcb70f345f2e21d4dae8bb472e6a3a0f
brandin888 <36492528+brandin888@users.noreply.github.com>
2018-02-15 14:46:30 -0500

    Initial commit

---
+++ src/api/config.js
@@ -0,0 +1,12 @@
+export default {
+    appName: 'platzimusic',
+    apiKey:'f105c82b9562998c14d2c52a5347e7c1',
+    secret:'0ce6ba9d4f0fb2f6feb77ddbdabfd311',
+    registeredTo:'brandin888'
+}
+
+
+//Application name	platzimusic
+//API key	f105c82b9562998c14d2c52a5347e7c1
+//Shared secret	0ce6ba9d4f0fb2f6feb77ddbdabfd311
+//Registered to	brandin888
